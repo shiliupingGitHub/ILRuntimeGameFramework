e2672394cc891d9c18f66aaf851570ac8b80b925
shiliupingGithub <582441842@qq.com>
2017-06-23 13:34:04 +0800

    asdasdasd

--- Game/Assets/Game/Scripts/main.cs
+++ Game/Assets/Game/Scripts/main.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 using ILRuntime;
+using System.Reflection;
+using System;
 public class main : MonoBehaviour {
 
     public enum RunType
@@ -10,11 +12,34 @@ public class main : MonoBehaviour {
     }
     public RunType mRunType = RunType.ILRuntime;
 	void Start () {
-	    
+	    switch(mRunType)
+        {
+            case RunType.MONO:
+                RunAsMono();
+                break;
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+    void RunAsMono()
+    {
+        CallMethod("Entrance", "Main");
+    }
+    void CallMethod(string typeName,string methodname)
+    {
+        GetAssembly(typeName).GetType(typeName).GetMethod(methodname).Invoke(null,null);
+    }
+    Assembly GetAssembly(string szName)
+    {
+        Assembly[] asms = AppDomain.CurrentDomain.GetAssemblies();
+        foreach(var a in asms)
+        {
+            if (a.ManifestModule.Name == szName + ".dll")
+                return a;
+        }
+        return null;
+    }
 }
