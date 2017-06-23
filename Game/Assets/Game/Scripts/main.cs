a22ebff014b298a6518836cb48e6e7e9f3cba8a7
shiliupingGithub <582441842@qq.com>
2017-06-23 11:46:51 +0800

    sadasdasd

---
+++ Game/Assets/Game/Scripts/main.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using ILRuntime;
+public class main : MonoBehaviour {
+
+    public enum RunType
+    {
+        MONO,
+        ILRuntime,
+    }
+    public RunType mRunType = RunType.ILRuntime;
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
